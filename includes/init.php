f0a1f90e4d6353ab63df037f7e8048b1eb7710aa
ShakassOne <alan@shakass.com>
2025-06-16 21:33:21 +0200

    Add initial admin pages

--- includes/init.php
+++ includes/init.php
@@ -1,17 +1,5 @@
 <?php
-// Hook de démarrage
-add_action('admin_menu', function () {
-    $icon_path = WINSHIRT_PATH . 'assets/logo.svg';
-    $icon_data_uri = 'dashicons-tshirt';
-    if (file_exists($icon_path)) {
-        $icon_data_uri = 'data:image/svg+xml;base64,' . base64_encode(file_get_contents($icon_path));
-    }
-    add_menu_page('WinShirt', 'WinShirt', 'manage_options', 'winshirt', 'winshirt_admin_page', $icon_data_uri, 56);
-});
 
-function winshirt_admin_page() {
-    echo '<div class="wrap"><h1>WinShirt - Dashboard</h1><p>Bienvenue sur le back-office WinShirt.</p></div>';
-}
 
 // Enqueue assets on product pages
 add_action('wp_enqueue_scripts', function () {
